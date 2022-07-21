88c31aba13a4971fcbc5c5e7000f763138bf200a
CosmoArt <cosmohydra17@gmail.com>
2022-07-21 13:56:16 -0500

    form styled 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Footer from './components/Footer';
 export default function App() {
     return (
         <>
-            <article className='md:text-left'>
-                <h1 className='text-2xl font-bold px-6 my-4'>Learn to code by watching others</h1>
+            <article className='md:text-left w-2/4'>
+                <h1 className='text-2xl font-bold px-6 my-4 md:text-4xl'>Learn to code by watching others</h1>
                 <p className='font-normal text-sm text-white text-opacity-70 my-4'>
                     See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
                     but understanding how developers think is invaluable.
@@ -14,8 +14,8 @@ export default function App() {
 
             </article>
 
-            <div>
-                <p className='rounded-md bg-pBlue p-4 text-sm px-10 my-6 shadow-pShadow'><strong>Try it free 7 days</strong> then $20/mo. thereafter</p>
+            <div className='w-2/4'>
+                <p className='rounded-md bg-pBlue p-5 text-sm px-10 mt-7 mb-4 shadow-pShadow leading-4'><strong>Try it free 7 days</strong> then $20/mo. thereafter</p>
                 <Form />
             </div>
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,19 +5,23 @@ const InputF = styled.input`
     padding: 10px;
     border-radius:3px;
     &::placeholder{
-        font-weight: 600;
+        font-weight: 500;
     }
     `
 export default function Form() {
 
     return (
         <form className='flex gap-3 flex-col p-5 bg-white shadow-pShadow rounded-md'>
-            <InputF type="text" name="firstName" placeholder="First Name" />
-            <InputF type="text" name="lastName" placeholder="Last Name" />
-            <InputF type="mail" name="email" placeholder="Email Address" />
-            <InputF type="password" name="password" placeholder="Password" />
-            <button type="submit" className='bg-pGreen'>Claim your free trial</button>
-            <p className='text-black text-opacity-50'>By clicking the button, you are agreeing to our <a href="https://www.example.com">Terms and Services</a></p>
+            <InputF type="text" id='firstName' name="firstName" placeholder="First Name" required/>
+            <label htmlFor="firstName" aria-live='true'></label>
+            <InputF type="text" name="lastName" id='lastName' placeholder="Last Name" required />
+            <label htmlFor="lastName" aria-live='true'></label>
+            <InputF type="mail" name="email" id='mail' placeholder="Email Address" required />
+            <label htmlFor="mail" aria-live='true'></label>
+            <InputF type="password" name="password" id='password' placeholder="Password" required />
+            <label htmlFor="password" aria-live='true'></label>
+            <button type="submit" className='bg-pGreen rounded-sm p-3' >Claim your free trial</button>
+            <footer className='text-black text-opacity-50 text-xs'>By clicking the button, you are agreeing to our <a href="https://www.example.com" className='text-pRed'>Terms and Services</a></footer>
         </form>
     )
 }
