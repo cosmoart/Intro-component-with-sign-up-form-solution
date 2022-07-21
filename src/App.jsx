df346e4e95ef3e1afee402650b747fed50a1eea1
CosmoArt <cosmohydra17@gmail.com>
2022-07-21 16:16:38 -0500

    form validation #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import Form from './components/Form';
 import Footer from './components/Footer';
+import { useRef } from 'react';
 
 export default function App() {
-    return (
-        <>
-            <article className='md:text-left w-2/4'>
-                <h1 className='text-2xl font-bold px-6 my-4 md:text-4xl'>Learn to code by watching others</h1>
-                <p className='font-normal text-sm text-white text-opacity-70 my-4'>
-                    See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
-                    but understanding how developers think is invaluable.
-                </p>
 
-            </article>
+	return (
+		<>
+			<article className='md:text-left md:w-2/4'>
+				<h1 className='text-2xl font-bold px-6 my-4 md:text-4xl md:px-0'>Learn to code by watching others</h1>
+				<p className='font-normal text-sm text-white text-opacity-70 my-4'>
+					See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
+					but understanding how developers think is invaluable.
+				</p>
 
-            <div className='w-2/4'>
-                <p className='rounded-md bg-pBlue p-5 text-sm px-10 mt-7 mb-4 shadow-pShadow leading-4'><strong>Try it free 7 days</strong> then $20/mo. thereafter</p>
-                <Form />
-            </div>
+			</article>
 
-            <Footer />
-        </>
-    );
+			<div className='md:w-2/4'>
+				<p className='rounded-md bg-pBlue p-5 text-sm px-10 mt-7 mb-4 shadow-pShadow leading-4'><strong>Try it free 7 days</strong> then $20/mo. thereafter</p>
+				<Form />
+			</div>
+
+			<Footer />
+		</>
+	);
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 
 const InputF = styled.input`
@@ -7,21 +7,50 @@ const InputF = styled.input`
     &::placeholder{
         font-weight: 500;
     }
-    `
+`
+
+const LabelF = styled.label`
+    text-align: right;
+    color: hsl(0, 100%, 74%);
+    font-size: 14px;
+    transform: translateY(-20px);
+    opacity: 0;
+    transition: all 0.2s ease-in-out;
+`
+
 export default function Form() {
+    const form = useRef(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // list of the elements minus the last
+        console.log(form.current.querySelectorAll('input'));
+        for (let i of form.current.querySelectorAll('input')) {
+            if (i.value === '') {
+                console.log(i.nextElementSibling);
+                // i.nextElementSibling.textContent = `${i.placeholder} cannot be empty`;
+                i.nextElementSibling.classList.add("form-error");
+                // document.querySelector(`label[for="${i.name}"]`).classList.add('error');
+            }
+        }
+    }
 
     return (
-        <form className='flex gap-3 flex-col p-5 bg-white shadow-pShadow rounded-md'>
-            <InputF type="text" id='firstName' name="firstName" placeholder="First Name" required/>
-            <label htmlFor="firstName" aria-live='true'></label>
+        <form ref={form} className='flex gap-3 flex-col p-5 bg-white shadow-pShadow rounded-md'>
+            <InputF type="text" id='firstName' name="firstName" placeholder="First Name" required />
+            <LabelF htmlFor="firstName" aria-live='true'>afwagwgwgwa</LabelF>
+
             <InputF type="text" name="lastName" id='lastName' placeholder="Last Name" required />
-            <label htmlFor="lastName" aria-live='true'></label>
+            <LabelF htmlFor="lastName" aria-live='true' />
+
             <InputF type="mail" name="email" id='mail' placeholder="Email Address" required />
-            <label htmlFor="mail" aria-live='true'></label>
+            <LabelF htmlFor="mail" aria-live='true' />
+
             <InputF type="password" name="password" id='password' placeholder="Password" required />
-            <label htmlFor="password" aria-live='true'></label>
-            <button type="submit" className='bg-pGreen rounded-sm p-3' >Claim your free trial</button>
-            <footer className='text-black text-opacity-50 text-xs'>By clicking the button, you are agreeing to our <a href="https://www.example.com" className='text-pRed'>Terms and Services</a></footer>
+            <LabelF htmlFor="password" aria-live='true' />
+
+            <button type="submit" className='bg-pGreen rounded-sm p-3' onClick={e => handleSubmit(e)}>Claim your free trial</button>
+            <footer className='text-black text-opacity-50 text-xs font-bold'>By clicking the button, you are agreeing to our <a href="#terms" className='text-pRed'>Terms and Services</a></footer>
         </form>
     )
 }
