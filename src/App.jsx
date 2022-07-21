8616334d76b010c1d0ec9f91447e73c69ca130b6
CosmoArt <cosmohydra17@gmail.com>
2022-07-21 18:40:16 -0500

    form validation #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
 import Footer from './components/Footer';
-import { useRef } from 'react';
 
 export default function App() {
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,14 @@
 import React, { useRef } from 'react';
 import styled from 'styled-components';
 
+const EMAILREGEX = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+
 const InputF = styled.input`
     padding: 10px;
+	border: 2px solid #3e3c4920;
     border-radius:3px;
+	color: hsl(249, 10%, 26%);
+	transition: border-color 0.2s ease-in-out;
     &::placeholder{
         font-weight: 500;
     }
@@ -13,44 +18,56 @@ const LabelF = styled.label`
     text-align: right;
     color: hsl(0, 100%, 74%);
     font-size: 14px;
-    transform: translateY(-20px);
-    opacity: 0;
+    transform: translateY(-40px);
+    /* opacity: 0; */
+	height: 0;
     transition: all 0.2s ease-in-out;
 `
 
 export default function Form() {
-    const form = useRef(null);
+	const form = useRef(null);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // list of the elements minus the last
-        console.log(form.current.querySelectorAll('input'));
-        for (let i of form.current.querySelectorAll('input')) {
-            if (i.value === '') {
-                console.log(i.nextElementSibling);
-                // i.nextElementSibling.textContent = `${i.placeholder} cannot be empty`;
-                i.nextElementSibling.classList.add("form-error");
-                // document.querySelector(`label[for="${i.name}"]`).classList.add('error');
-            }
-        }
-    }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const $mail = form.current.querySelector("#mail");
 
-    return (
-        <form ref={form} className='flex gap-3 flex-col p-5 bg-white shadow-pShadow rounded-md'>
-            <InputF type="text" id='firstName' name="firstName" placeholder="First Name" required />
-            <LabelF htmlFor="firstName" aria-live='true'>afwagwgwgwa</LabelF>
+		for (let i of form.current.querySelectorAll('input')) {
+			if (i.value === '') {
+				i.nextElementSibling.textContent = `${i.placeholder} cannot be empty`;
+				i.nextElementSibling.classList.add("form-error");
+				form.current.querySelector(`input[name="${i.name}"]`).style.border = "2px solid hsl(0, 100%, 74%)";
+			} else {
+				i.nextElementSibling.textContent = "";
+				i.nextElementSibling.classList.remove("form-error");
+				form.current.querySelector(`input[name="${i.name}"]`).style.border = "2px solid #3e3c49";
+			}
+		}
+		if (!$mail.value.match(EMAILREGEX) && $mail.value !== '') {
+			$mail.nextElementSibling.textContent = "Looks like this is not an email";
+			$mail.nextElementSibling.classList.add("form-error");
+			return;
+		}
+		if (form.current.querySelectorAll('.form-error').length === 0) {
+			form.current.submit();
+		}
+	}
 
-            <InputF type="text" name="lastName" id='lastName' placeholder="Last Name" required />
-            <LabelF htmlFor="lastName" aria-live='true' />
+	return (
+		<form ref={form} className='flex gap-3 flex-col p-5 bg-white shadow-pShadow rounded-md'>
+			<InputF type="text" id='firstName' name="firstName" placeholder="First Name" required />
+			<LabelF htmlFor="firstName" aria-live='true'></LabelF>
 
-            <InputF type="mail" name="email" id='mail' placeholder="Email Address" required />
-            <LabelF htmlFor="mail" aria-live='true' />
+			<InputF type="text" name="lastName" id='lastName' placeholder="Last Name" required />
+			<LabelF htmlFor="lastName" aria-live='true' />
 
-            <InputF type="password" name="password" id='password' placeholder="Password" required />
-            <LabelF htmlFor="password" aria-live='true' />
+			<InputF type="mail" name="email" id='mail' placeholder="Email Address" required />
+			<LabelF htmlFor="mail" aria-live='true' />
 
-            <button type="submit" className='bg-pGreen rounded-sm p-3' onClick={e => handleSubmit(e)}>Claim your free trial</button>
-            <footer className='text-black text-opacity-50 text-xs font-bold'>By clicking the button, you are agreeing to our <a href="#terms" className='text-pRed'>Terms and Services</a></footer>
-        </form>
-    )
+			<InputF type="password" name="password" id='password' placeholder="Password" required />
+			<LabelF htmlFor="password" aria-live='true' />
+
+			<button type="submit" className='bg-pGreen rounded-sm p-3 font-semibold uppercase' onClick={e => handleSubmit(e)}>Claim your free trial</button>
+			<footer className='text-black text-opacity-50 text-xs font-bold'>By clicking the button, you are agreeing to our <a href="#terms" className='text-pRed'>Terms and Services</a></footer>
+		</form>
+	)
 }
